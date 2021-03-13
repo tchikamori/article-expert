edc823352129bbac006bb59ce38522575a2b6094
Takao Chikamori <takao.chikamori@gmail.com>
2021-03-13 11:20:48 +0900

    PWA enabled

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import 'primevue/resources/themes/saga-blue/theme.css'       //theme
 import 'primevue/resources/primevue.min.css'                 //core css
 import 'primeicons/primeicons.css'                           //icons
 import 'primeflex/primeflex.css';
+import './registerServiceWorker'
 
 
 const app = createApp(App)
